15a79c8db795914d94783bc965f556a0dbcd2c7d
Turya <allindiaturya.kalburgi@gmail.com>
2026-07-31 21:13:13 +0530

    Time: 12 ms (44.96%), Space: 46 MB (79.15%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int[] count = new int[26];
+        int left = 0;
+        int maxcount = 0;
+        int maxlen = 0;
+
+        //outer loop
+        for(int right = 0; right < s.length(); right++){
+
+           count[s.charAt(right) - 'A']++;
+
+           maxcount = Math.max(maxcount,count[s.charAt(right) - 'A']);
+
+           if((right - left + 1) - maxcount > k){
+
+            count[s.charAt(left) - 'A']--;
+            left++;
+
+           }
+
+           maxlen = Math.max(maxlen, right -left + 1);
+
+
+        }
+        return maxlen;
+    }
+}

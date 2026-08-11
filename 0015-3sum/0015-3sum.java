b97d73e2aaed3126ad1a714d00c9a2c289e61acd
Turya <allindiaturya.kalburgi@gmail.com>
2026-08-11 08:09:02 +0530

    Time: 34 ms (60.7%), Space: 59.3 MB (45.73%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
+        
+        //ArrayList
         List<List<Integer>> result = new ArrayList<>();
 
         //sort
         Arrays.sort(nums);
 
-        //outerlop
-        for(int i=0; i<nums.length-2; i++){
-            
-            //duplicate skip
-            if(i>0 && nums[i] == nums[i-1]) continue;
+        //outer loop
+        for(int i = 0; i<nums.length -2; i++){
 
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            //left right
             int left = i+1;
-
             int right = nums.length-1;
 
+            //while
             while(left < right){
+                
                 //int sum
                 int sum = nums[i] + nums[left] + nums[right];
 
-                //conditions while
+                //3 conditions
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
-
-                    //skip left duplicate
-                    while(left < right && (nums[left] == nums[left+1])) left++;
-
-                    //skip for right
-                    while(left < right &&( nums[right] == nums[right-1])) right--;
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // no duplicates!
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
 
                     left++;
                     right--;
-
-                }else if(sum<0){
+                } else if (sum < 0){
                     left++;
-                }else{
-                    right--;
+                    }else{
+                        right--;
+                    }
                 }
-            }
         }
-
-        return result;
-        
+       return result;
     }
 }

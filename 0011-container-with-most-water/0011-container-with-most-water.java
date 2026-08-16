c4541336adcf60c2418078dd5221ebbde866bd8c
Turya <allindiaturya.kalburgi@gmail.com>
2026-08-16 20:31:03 +0530

    Time: 5 ms (82.98%), Space: 77.4 MB (49.49%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,26 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-
-        //inilize
-        int left = 0;
+        
+        int left =0;
         int right = height.length-1;
         int maxArea = 0;
 
-        //condition
-        while(left < right){
+        while(left<right){
 
-            //formula
-            int area= Math.min(height[left], height[right]) * (right - left);
+            int area = Math.min(height[left], height[right]) * (right -left);
 
-            //MaxArea
+            //update maxarrea
             maxArea = Math.max(maxArea, area);
 
-            //cases
-            if(height[left] < height[right]){
+            if(height[left]< height[right]){
                 left++;
+
             }else{
                 right--;
             }
+
         }
         return maxArea;
     }

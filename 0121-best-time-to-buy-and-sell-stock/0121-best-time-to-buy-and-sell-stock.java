914b7c408e25eb1bcc96f88c2472f1c0ea26b1f8
Turya <allindiaturya.kalburgi@gmail.com>
2026-08-31 21:34:51 +0530

    Time: 1 ms (99.95%), Space: 94 MB (94.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-        //intialize
-        int minprice = prices[0];
+        int minprices = prices[0];
+
         int maxprofit = 0;
 
-        //outer loop
         for(int i=0; i<prices.length; i++){
 
-            //formula
-            if(prices[i] < minprice){
-                minprice = prices[i];
-            }else{
-                maxprofit = Math.max(maxprofit, prices[i] - minprice);
-            }
+            minprices = Math.min(minprices, prices[i]);
+
+            int profit = prices[i] - minprices;
+
+            maxprofit = Math.max(maxprofit, profit);
         }
         return maxprofit;
     }

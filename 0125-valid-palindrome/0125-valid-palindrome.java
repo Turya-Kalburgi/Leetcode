40cd157421900f14a35a4623fae5ce832fa21561
Turya <allindiaturya.kalburgi@gmail.com>
2026-09-03 00:20:23 +0530

    Time: 2 ms (99.3%), Space: 44.2 MB (83.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        //initialize
-        int left = 0; //O(1)
+        int left =0;
+        int right = s.length()-1;
 
-        int right = s.length()-1;  //O(1)
+        while(left < right){
 
-        //condition
-        while( left < right){ //O(n), GOESE TO N/2 
-
-            //skip non alphanumerical on left
-            while( left < right && !Character.isLetterOrDigit(s.charAt(left))) left++; //O(n)
-
-            //skip non alphanumerical on right
-            while( left < right && !Character.isLetterOrDigit(s.charAt(right))) right--; //O(n)
-
-            //to lower case and not equal return false
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) { //O(1)
-                return false;  //O(1)
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
             }
-             
-             left++;    //O(1)
-             right--;   //O(1)
+
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) !=
+            (Character.toLowerCase(s.charAt(right)))){
+                return false;
+            }
+            left++;
+            right--;
         }
-        return true;   //O(1)
+        return true;
+        
     }
 }
